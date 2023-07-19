4342885ec3f77ca37b26fd1a5fa0a2200236edb4
Mandana Naderi <naderimandana1401@gmail.com>
2023-07-19 11:22:27 +0330

    added exersizes of week 7

---
+++ Week 7.py
@@ -0,0 +1,42 @@
+
+
+def ashar(n):
+    before, after = round(2.3, 2), round((2.3 + 0.01), 2)
+    for _ in range(n):
+        if before == round(2.3, 2):
+            yield before
+        if after == round(3.79, 2):
+            break
+        else:
+            yield after
+            before, after = round(after, 2), round((after + 0.01), 2)
+
+
+alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+def halfalphabet():
+    per, cur = alphabet[2], alphabet[4]
+
+    for _ in alphabet:
+        if per == 'C':
+            yield per
+        if cur == 'Y':
+            break
+        else:
+            yield cur
+            per, cur = cur, alphabet[alphabet.index(cur)+2]
+
+
+if __name__ == '__main__':
+
+    asharList = []
+    for number in ashar(1000):
+        asharList.append(number)
+    print(asharList)
+    print(len(asharList))
+
+    print('--------------------------------')
+
+    for _ in halfalphabet():
+        print(_)
+
+
